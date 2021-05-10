2d6a227b3b429da1804a98f77e096620e8247e2e
TheDoctorWhoo <olexa99@gmail.com>
2021-05-10 22:38:32 +0300

    Task 4 completed.

--- wpfLab/MainWindow.xaml.cs
+++ wpfLab/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,21 @@ namespace wpfLab
         public MainWindow()
         {
             InitializeComponent();
+            //Создание привязки и присоединение обработчиков
+            CommandBinding saveCommand = new CommandBinding(ApplicationCommands.Save, execute_Save, canExecute_Save);
+            //Регистрация привязки
+            CommandBindings.Add(saveCommand);
+
         }
+        void canExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (inputTextBox.Text.Trim().Length > 0) e.CanExecute = true; else e.CanExecute = false;
+        }
+        void execute_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            System.IO.File.WriteAllText("d:\\myFile.txt", inputTextBox.Text);
+            MessageBox.Show("The file was saved!");
+        }
+
     }
 }
